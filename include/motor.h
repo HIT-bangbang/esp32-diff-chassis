df8c75092c9b76e65415db81f44fd3c15d0fdc96
HIT-bangbang <2053767692@qq.com>
2024-10-29 17:22:32 +0800

    修改电机驱动为EG2104

--- include/motor.h
+++ include/motor.h
@@ -10,12 +10,14 @@ private:
     uint8_t input_pin_2;
     uint8_t resolution;
     int max_pwm_value;
-    uint8_t channel;
+    uint8_t channel_1;
+    uint8_t channel_2;
+
     int freq;
 
 
 public:
-    Motor(uint8_t _en_pin, uint8_t _input_pin_1,uint8_t _input_pin_2, bool _direction, uint8_t _channel, uint8_t _resolution, int _freq);
+    Motor(uint8_t _en_pin, uint8_t _input_pin_1,uint8_t _input_pin_2, bool _direction, uint8_t _channel_1,uint8_t _channel_2, uint8_t _resolution, int _freq);
     void init();
     void run(float ratio);
     void stop();
